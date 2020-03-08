7d032440dc3c684608395b49d4f065c36923d42b
fazekmar <fazekmar@gmail.com>
2020-03-08 12:58:05 +0100

    Fix shellcheck errors and update packages

--- host/install-host.sh
+++ host/install-host.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ ! $(command -v python3) ]
+if [ ! "$(command -v python3)" ]
 then
     echo "Python3 not found, but it's needed to run the host application!"
     exit 1
 fi
 
-if [ $(uname -s) = "Darwin" ];
+if [ "$(uname -s)" = "Darwin" ]
 then
     INSTALL_DIR="${HOME}/Library/Application Support/Mozilla/NativeMessagingHosts"
 else
@@ -25,12 +25,13 @@ EOF
 )
 
 # Create native messaging dir if does not exist
-if [ ! -d "${INSTALL_DIR}" ]; then
+if [ ! -d "${INSTALL_DIR}" ]
+then
     mkdir -p "${INSTALL_DIR}"
 fi
 
 # Create App manifest
-echo ${HOST_DESC} > "${INSTALL_DIR}/yastm.json"
+echo "${HOST_DESC}" > "${INSTALL_DIR}/yastm.json"
 
 # Download App side
 curl -sSL https://raw.githubusercontent.com/fazekmar/yastm/master/host/yastm-host.py > "${INSTALL_DIR}/yastm.py"
